bb5ca7ea4528a8d8992718320928af8e4cf653dc
Madhav Kwatra <87752844+MadhavKwatra@users.noreply.github.com>
2026-05-09 23:54:12 +0530

    Fixing some issues because of build errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
 import EventCard from "@/components/EventCard";
 import ExploreBtn from "@/components/ExploreBtn";
 import { IEvent } from "@/database";
-// import { events } from "@/lib/constants";
+import { cacheLife } from "next/cache";
+import { events } from "@/lib/constants";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const HomePage = async () => {
-  const response = await fetch(`${BASE_URL}/api/events`);
-  const { events } = await response.json();
+  "use cache";
+  cacheLife("hours");
+  // const response = await fetch(`${BASE_URL}/api/events`);
+  // const { events } = await response.json();
 
   return (
     <section>
@@ -25,7 +28,8 @@ const HomePage = async () => {
         <ul className="events">
           {events &&
             events.length > 0 &&
-            events.map((event: IEvent) => (
+            // events.map((event:IEvent) => (
+            events.map((event) => (
               <li key={event.title} className="list-none">
                 <EventCard {...event} />
               </li>
